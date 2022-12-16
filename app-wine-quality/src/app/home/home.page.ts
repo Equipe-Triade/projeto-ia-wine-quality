5132146b9cc5df8308ebf3aa9081d49a9390d8dd
Sophia Martins <79290630+sophiafmartins@users.noreply.github.com>
2022-12-15 22:18:48 -0300

    commit com arquivos - parte 2

--- app-wine-quality/src/app/home/home.page.ts
+++ app-wine-quality/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,52 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  resultado = false;
+  carregando = false;
+  btn_upload = true;
+  formData = new FormData();
+  resultado_modelo: any = {};
+
+
+  public async passandoArquivoParaAPI(){
+
+    this.carregando = true;
+    this.resultado = true;
+
+    this.resultado_modelo = await this.http.post("http://127.0.0.1:5000/quality-wine/", this.formData).toPromise();
+    console.log(this.resultado_modelo);
+    
+    
+    this.carregando = false;
+    
+      
+
+  }
+
+  
+  public carregandoArquivoDoDispositivo(event: any){
+    const d = new FormData();
+    if (event.target.files && event.target.files[0]){
+      const file = event.target.files[0];
+      
+      d.append('arquivo', file);
+      this.formData = d;
+
+      this.btn_upload = false;
+
+    }
+
+  }
+
+
+
+
+
+
+
+
+
 
 }
